b5ac0c5f6287032e20a0b1e4e5cf782aef6593d7
azesm <azesm@yahoo.com>
2024-06-01 17:30:58 +0300

    hello

--- src/www/vladan/codewars/eight/HelloWorld.java
+++ src/www/vladan/codewars/eight/HelloWorld.java
@@ -1,2 +1,24 @@
-package www.vladan.codewars;public class HelloWorld {
+package www.vladan.codewars.eight;
+
+/*
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+Создайте простую функцию под названием «приветствие», которая возвращает самое известное «привет, мир!».
+Очки стиля
+Конечно, это настолько просто, насколько это возможно. Но насколько вы умны, чтобы создать самый креативный «привет, мир», который только можно придумать? Какое решение «привет, мир» вы бы хотели показать своим друзьям?
+*/
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String first = "hello ";
+        String second = "world!";
+
+        String greet = first + second;
+        System.out.println(greet);
+
+    }
 }
